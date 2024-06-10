135ae080bcd8cf0522859b284dc17008c370b0d8
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-10 21:54:55 +0300

    Initial commit

---
+++ TrapezoidArea/Program.cs
@@ -0,0 +1,11 @@
+﻿class TrapezoidArea
+{
+    static void Main(string[] args)
+    {
+        int firstBase = int.Parse(Console.ReadLine());
+            int secondBase=int.Parse(Console.ReadLine());
+            int height=int.Parse(Console.ReadLine());
+            int area=(firstBase + secondBase) / 2 * height;
+            Console.WriteLine(area);
+    }
+    }
